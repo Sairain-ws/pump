f61296a89bc11a7c2ba171ce9de3a901a1780205
lgc <1354859496@qq.com>
2024-06-18 18:38:17 +0800

    test666

--- pump/ContentView.swift
+++ pump/ContentView.swift
@@ -77,7 +77,7 @@ struct ContentView: View {
                 
                 Text("我已阅读并同意").font(Font.custom("Arial", size: 12)).lineLimit(1)
                 Button(action: {
-                    print("打开免责声明");
+                    print("打开免责声明666");
                 }){
                     Text("《用户协议与免责声明》")
                         .font(Font.custom("Arial", size: 12)).lineLimit(1)
